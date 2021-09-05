a0071dedf28d6abba1c4622090f2417115026195
afreen505 <afreen.fathimaa9876@gmail.com>
2021-09-05 20:31:28 +0530

    Added: Welcome Message

---
+++ Test/registration/UserRegistrationTest.java
@@ -0,0 +1,116 @@
+package registration;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+    UserRegistration user = null;
+
+    @Before
+    public void setUp() {
+        user = new UserRegistration();
+    }
+
+    @Test
+    public void firstNameTest_should_giveTrueWhenNameIsValid() {
+        boolean result = user.validateFirstName("Afreen");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void firstNameTest_should_giveFalseWhenName_doesnotStartWithUpperCase() {
+        boolean result = user.validateFirstName("afreen");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void firstNameTest_should_giveTrueWhenName_containsMinimumThreeCharacters() {
+        boolean result = user.validateFirstName("Afr");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void firstNameTest_should_giveFalse_whenAllCharactersOfNameInUpperCase() {
+        boolean result = user.validateFirstName("FATHIMA");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void firstNameTest_should_giveFalse_whenAllCharactersOfNameInLowerCase() {
+        boolean result = user.validateFirstName("afreen");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void lastNameTest_should_giveTrueWhenNameIsValid() {
+        boolean result = user.validateLastName("fathima");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void lastNameTest_should_giveFalseWhenName_doesnotStartWithUpperCase() {
+        boolean result = user.validateLastName("afreen");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void lastNameTest_should_giveTrueWhenName_containsMinimumThreeCharacters() {
+        boolean result = user.validateLastName("afr");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void lastNameTest_should_giveFalse_whenAllCharactersOfNameInUpperCase() {
+        boolean result = user.validateLastName("AFREEN");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void lastNameTest_should_giveFalse_whenAllCharactersOfNameInLowerCase() {
+        boolean result = user.validateLastName("fathima");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void mobileNumberTest_should_giveTrue_whenMobileeNumberIsValid() {
+        boolean result = user.validateMobileNo("91 987654321");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void mobileNumberTest_should_giveFalse_whenMobileeNumberIsInValid() {
+        boolean result = user.validateMobileNo("987654321");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void mobileNumberTest_should_giveFalse_whenNoSpaceBetween_mobileeNumberAndCountryCode() {
+        boolean result = user.validateMobileNo("987654321");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void passwordTest_should_giveTrue_whenPasswordIsValid() {
+        boolean result = user.validatePassword("oneFine$Day123");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void passwordTest_should_giveFalse_whenPasswordIsInValid() {
+        boolean result = user.validatePassword("oneFine$Day");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void emailIdTest_should_giveTrue_whenEmailIdIsValid() {
+        boolean result = user.validateEmail("abc.xyz@bl.co.in");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void emailIdTest_should_giveFalse_whenEmailIdIsInValid() {
+        boolean result = user.validateEmail("abc@@bl.in");
+        Assert.assertFalse(result);
+    }
+}
---
+++ main/registration/UserRegistration.java
@@ -0,0 +1,92 @@
+package registration;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public boolean validateFirstName(String name) {
+        String regex = "^[A-Z][a-z]{2,}";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(name);
+        boolean b = m.matches();
+        if (b) {
+            System.out.println("First Name of the user is valid.");
+            return true;
+        } else {
+            System.out.println("First Name of the user is invalid.");
+        }
+        return false;
+    }
+
+    public static boolean validateLastName(String name) {
+        String regex = "^[A-Z][a-z]{2,}";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(name);
+        boolean b = m.matches();
+        if (b) {
+            System.out.println("Last Name of the user is valid.");
+            return true;
+        } else {
+            System.out.println("Last Name of the user is invalid.");
+        }
+        return false;
+    }
+
+    /**
+     * UC-3 This method is validating the email id of the user is correct or not.
+     */
+    public static boolean validateEmail(String emailId) {
+        String regex = "^[a-z0-9.+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(emailId);
+        boolean b = m.matches();
+        if (b) {
+            System.out.println("Email Id of the user is valid.");
+            return true;
+        } else {
+            System.out.println("Email Id of the user is invalid.");
+        }
+        return false;
+
+    }
+
+    /**
+     * UC-4 This method is validating the Mobile number of the user is correct or
+     * not.
+     */
+    public static boolean validateMobileNo(String mobileNumber) {
+        String regex = "[0-9]{2}\\s{1}[0-9]{10}";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(mobileNumber);
+        boolean b = m.matches();
+        if (b) {
+            System.out.println("Mobile number of the user is valid.");
+            return true;
+        } else {
+            System.out.println("Mobile number of the user is invalid.");
+        }
+        return false;
+
+    }
+
+    public static boolean validatePassword(String password) {
+        String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(password);
+        boolean b = m.matches();
+        if (b) {
+            System.out.println("password of the user is valid.");
+            return true;
+        } else {
+            System.out.println("password of the user is invalid.");
+        }
+        return false;
+
+    }
+
+}
+

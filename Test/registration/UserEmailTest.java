d062a12185f5e5c3315467819954429f783de1a5
afreen505 <afreen.fathimaa9876@gmail.com>
2021-09-05 20:45:26 +0530

    Modified:Program executed Successfully and removed whitespaces and blanks

--- Test/registration/UserEmailTest.java
+++ Test/registration/UserEmailTest.java
@@ -1,2 +1,39 @@
-package registration;public class UserEmailTest {
+package registration;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+
+public class UserEmailTest {
+    private String email;
+    private boolean expectedResult;
+
+    public UserEmailTest(String email, boolean expectedResult) {
+        this.email = email;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Collection input() {
+        return Arrays.asList(new Object[][]{{"abc@yahoo.com", true}, {"abc-100@yahoo.om", true},
+                {"abc.100@yahoo.com", true}, {"abc111@abc.com", true}, {"abc-100@abc.net", true},
+                {"abc.100@abc.com.au", true}, {"abc@1.com", true}, {"abc@gmail.com.com", true},
+                {"abc+100@gmail.com", true}, {"abc", false}, {"abc@.com.my", false}, {"abc123@gmail.a", false},
+                {"abc123@.com", false}, {"abc123@.com.com", false}, {".abc@abc.com", false},
+                {"abc()*@gmail.com", false}, {"abc@%*.com", false}, {"abc..2002@gmail.com", false},
+                {"abc.@gmail.com", false}, {"abc@abc@gmail.com", false}, {"abc@gmail.com.1a", false},
+                {"abc@gmail.com.aa.au", false}});
+    }
+
+    @Test
+    public void givenEmailAsVariable_shouldReturn_asParameterizedResult() {
+        UserRegistration user = new UserRegistration();
+        boolean result = user.checkMultipleEmail(this.email);
+        Assert.assertEquals(this.expectedResult, result);
+    }
 }
--- main/registration/UserRegistration.java
+++ main/registration/UserRegistration.java
@@ -5,11 +5,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-
     Scanner scanner = new Scanner(System.in);
 
+    /**
+     * UC-1 This method is validating the first name of the user is correct or not.
+     */
     public boolean validateFirstName(String name) {
-        String regex = "^[A-Z][a-z]{3,}";
+        String regex = "^[A-Z][a-z]{2,}";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(name);
         boolean b = m.matches();
@@ -22,7 +24,10 @@ public class UserRegistration {
         return false;
     }
 
-    public static boolean validateLastName(String name) {
+    /**
+     * UC-2 This method is validating the last name of the user is correct or not.
+     */
+    public boolean validateLastName(String name) {
         String regex = "^[A-Z][a-z]{2,}";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(name);
@@ -39,7 +44,7 @@ public class UserRegistration {
     /**
      * UC-3 This method is validating the email id of the user is correct or not.
      */
-    public static boolean validateEmail(String emailId) {
+    public boolean validateEmail(String emailId) {
         String regex = "^[a-z0-9.+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(emailId);
@@ -58,7 +63,7 @@ public class UserRegistration {
      * UC-4 This method is validating the Mobile number of the user is correct or
      * not.
      */
-    public static boolean validateMobileNo(String mobileNumber) {
+    public boolean validateMobileNo(String mobileNumber) {
         String regex = "[0-9]{2}\\s{1}[0-9]{10}";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(mobileNumber);
@@ -73,7 +78,20 @@ public class UserRegistration {
 
     }
 
-    public static boolean validatePassword(String password) {
+    /**
+     * UC-5 This method is validating the first rule of password of the user is
+     * correct or not. It should minimum of 8 characters.
+     * <p>
+     * UC-6 This method is validating the second rule of password of the user is
+     * correct or not. It should contain at least one upper case.
+     * <p>
+     * UC-7 This method is validating the third rule of password of the user is
+     * correct or not. It should contain at least one numeric value.
+     * <p>
+     * UC-8 This method is validating the third rule of password of the user is
+     * correct or not. It should contain one special character.
+     */
+    public boolean validatePassword(String password) {
         String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(password);
@@ -88,5 +106,11 @@ public class UserRegistration {
 
     }
 
-}
+    /**
+     * UC-9 Validating the given sample emailId
+     */
+    public boolean checkMultipleEmail(String email) {
+        return email.matches("^[0-9a-zA-Z]+([+._-]{1}[0-9a-zA-Z]+)?@[0-9a-zA-Z]+[.]{1}[a-zA-z]{2,4}([.]{1}[a-zA-z]{2,3})?$");
+    }
 
+}
